8a2761f41019745d42a3d12de045af76908aaba0
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2018-01-31 20:34:54 +0100

    Split the benchmark in its own file

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,14 +1,28 @@
-#include <iostream>
-
 #include "unicode_view"
 #include "benchmark_util.h"
 
+#include <chrono>
+#include <iostream>
+
+enum {
+    BENCH_COUNT = 10000000
+};
+
 template <typename T>
-void printCodePoints(const T *codeUnits)
+void benchMark(const T *codeUnits)
 {
-    for (auto codePoint : kdab::make_unicode_view(codeUnits, codeUnits + std::char_traits<T>::length(codeUnits)))
-        std::cout << codePoint << " ";
-    std::cout << std::endl;
+    const auto length = std::char_traits<T>::length(codeUnits);
+    using clock = std::chrono::steady_clock;
+    auto view = kdab::make_unicode_view(codeUnits, codeUnits + length);
+
+    auto start = clock::now();
+    for (int i = 0; i < BENCH_COUNT; ++i) {
+        for (auto codePoint : view)
+          escape(&codePoint);
+    }
+    clobber();
+    auto stop = clock::now();
+    std::cout << "Benchmarking " << length << " code uints of utf" << 8 * sizeof(T) << " took:" << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms " << std::endl;
 }
 
 int main()
@@ -16,17 +30,9 @@ int main()
     const char     str8[]  = u8"€Hello𐐷7バイト以上の文字は規定されないため、0xFE、0xFF (BOM) に0xFEと0xFFを使用するUTF-16やUTF-32が、UTF-8と混同されることはない。UTF-16 is specified in the latest versions of both the international standard ISO/IEC 10646 and the Unicode Standard.";
     const char16_t str16[] = u"€Hello𐐷7バイト以上の文字は規定されないため、0xFE、0xFF (BOM) に0xFEと0xFFを使用するUTF-16やUTF-32が、UTF-8と混同されることはない。UTF-16 is specified in the latest versions of both the international standard ISO/IEC 10646 and the Unicode Standard.";
     const char32_t str32[] = U"€Hello𐐷7バイト以上の文字は規定されないため、0xFE、0xFF (BOM) に0xFEと0xFFを使用するUTF-16やUTF-32が、UTF-8と混同されることはない。UTF-16 is specified in the latest versions of both the international standard ISO/IEC 10646 and the Unicode Standard.";
-    const char     *_str8  = u8"€Hello𐐷";
-    const char16_t *_str16 = u"€Hello𐐷";
-    const char32_t *_str32 = U"€Hello𐐷";
 
-    std::cout << "Size in memory " << sizeof str8 << " " <<  sizeof str16 << " " <<  sizeof str32 << std::endl;
-    std::cout << "Test unicode_view" << std::endl;
-    std::cout << std::hex;
-    printCodePoints(str8);
-    printCodePoints(str16);
-    printCodePoints(str32);
-    printCodePoints(_str8);
-    printCodePoints(_str16);
-    printCodePoints(_str32);
+    std::cout << "benchmarking ..." << std::endl;
+    benchMark(str8);
+    benchMark(str16);
+    benchMark(str32);
 }
