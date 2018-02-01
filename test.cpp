323b4782d624087c2e08bfef1cde1bdc5e258c93
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2018-02-02 00:36:42 +0100

    Rearrange the code so to more easily provide make_trusted_unicode_view

--- test.cpp
+++ test.cpp
@@ -12,13 +12,21 @@ TEST_CASE("unicode_view")
         const char16_t str16[] = u"€Hello𐐷7バイト以上の文字は規定されないため、0xFE、0xFF (BOM) に0xFEと0xFFを使用するUTF-16やUTF-32が、UTF-8と混同されることはない。UTF-16 is specified in the latest versions of both the international standard ISO/IEC 10646 and the Unicode Standard.";
         const char32_t str32[] = U"€Hello𐐷7バイト以上の文字は規定されないため、0xFE、0xFF (BOM) に0xFEと0xFFを使用するUTF-16やUTF-32が、UTF-8と混同されることはない。UTF-16 is specified in the latest versions of both the international standard ISO/IEC 10646 and the Unicode Standard.";
 
-        const auto view8 = kdab::make_unicode_view(std::begin(str8), std::end(str8));
-        const auto view16 = kdab::make_unicode_view(std::begin(str16), std::end(str16));
-        const auto view32 = kdab::make_unicode_view(std::begin(str32), std::end(str32));
+        const auto safe_view8 = kdab::make_safe_unicode_view(std::begin(str8), std::end(str8));
+        const auto safe_view16 = kdab::make_safe_unicode_view(std::begin(str16), std::end(str16));
+        const auto safe_view32 = kdab::make_safe_unicode_view(std::begin(str32), std::end(str32));
 
-        REQUIRE( std::equal(std::begin(view8),  std::end(view8),  std::begin(str32), std::end(str32)) );
-        REQUIRE( std::equal(std::begin(view16), std::end(view16), std::begin(str32), std::end(str32)) );
-        REQUIRE( std::equal(std::begin(view32), std::end(view32), std::begin(str32), std::end(str32)) );
+        REQUIRE( std::equal(std::begin(safe_view8),  std::end(safe_view8),  std::begin(str32), std::end(str32)) );
+        REQUIRE( std::equal(std::begin(safe_view16), std::end(safe_view16), std::begin(str32), std::end(str32)) );
+        REQUIRE( std::equal(std::begin(safe_view32), std::end(safe_view32), std::begin(str32), std::end(str32)) );
+
+        const auto trusted_view8 = kdab::make_trusted_unicode_view(std::begin(str8), std::end(str8));
+        const auto trusted_view16 = kdab::make_trusted_unicode_view(std::begin(str16), std::end(str16));
+        const auto trusted_view32 = kdab::make_trusted_unicode_view(std::begin(str32), std::end(str32));
+
+        REQUIRE( std::equal(std::begin(trusted_view8),  std::end(trusted_view8),  std::begin(str32), std::end(str32)) );
+        REQUIRE( std::equal(std::begin(trusted_view16), std::end(trusted_view16), std::begin(str32), std::end(str32)) );
+        REQUIRE( std::equal(std::begin(trusted_view32), std::end(trusted_view32), std::begin(str32), std::end(str32)) );
     }
 
     SECTION("UTF-8 decoding") {
